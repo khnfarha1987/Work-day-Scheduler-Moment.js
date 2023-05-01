e5a3a7755c99f01976d1b338960d09ad02dbb1cf
Farha Khan <khurshidkhan@Khurshids-MacBook-Pro.local>
2023-05-01 16:41:09 +0100

    create array of objects and create function for time blocks.

--- script.js
+++ script.js
@@ -3,7 +3,34 @@ var $timeBlocks = $(".time-block");
 var $scheduleArea = $(".schedule");
 
 var toDoItems = [];
-//each object has a hour property and a text property
+//each object has a hour property and a text property:
 
 var currentDate = moment().format("dddd, MMMM Do");
 var currentHour = moment().format("H");
+
+//Create array of objects:
+function initializeSchedule() {
+    //  console.log(toDoItems);
+
+    //for each time block
+    $timeBlocks.each(function () {
+        var $thisBlock = $(this);
+        var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+
+        var todoObj = {
+            //set related todo hour to same as data-hour
+            hour: thisBlockHr,
+            //get text ready to accept string input
+            text: "",
+        }
+        //add this todo object to todoitems array
+        toDoItems.push(todoObj);
+    });
+
+    //once we have looped timeblocks then save this array of objects to local storage by stringifying it first
+    localStorage.setItem("todos", JSON.stringify(toDoItems));
+    //console.log(toDoItems);
+}
+
+
+
